6f2f933711472d6273da59a39bd5f63e338275e2
CERAXLAN <er_aslan95@outlook.com>
2022-02-05 20:19:58 +0300

    Readmee

--- CERAXLAN.Adesso/CERAXLAN.Adesso.Business/DependencyResolvers/AutoFac/ValidationModule.cs
+++ CERAXLAN.Adesso/CERAXLAN.Adesso.Business/DependencyResolvers/AutoFac/ValidationModule.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using CERAXLAN.Adesso.Business.ValidationRules.FluentValidation;
 using CERAXLAN.Adesso.Entities.Concrete;
+using CERAXLAN.Adesso.Entities.Request.Travel;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,7 @@ namespace CERAXLAN.Adesso.Business.DependencyResolvers.AutoFac
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<TravelValidator>().As<IValidator<Travel>>().SingleInstance();
+            builder.RegisterType<TravelValidator>().As<IValidator<CreateTravelRequestDTO>>().SingleInstance();
         }
     }
 }
--- CERAXLAN.Adesso/CERAXLAN.Adesso.Business/ValidationRules/FluentValidation/TravelValidator.cs
+++ CERAXLAN.Adesso/CERAXLAN.Adesso.Business/ValidationRules/FluentValidation/TravelValidator.cs
@@ -1,4 +1,5 @@
 ﻿using CERAXLAN.Adesso.Entities.Concrete;
+using CERAXLAN.Adesso.Entities.Request.Travel;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
@@ -8,13 +9,15 @@ using System.Threading.Tasks;
 
 namespace CERAXLAN.Adesso.Business.ValidationRules.FluentValidation
 {
-    public class TravelValidator : AbstractValidator<Travel>
+    public class TravelValidator : AbstractValidator<CreateTravelRequestDTO>
     {
         public TravelValidator()
         {
 
-            RuleFor(p => p.ToWhere).NotEqual("");
-            RuleFor(p => p.WhereFrom).NotEmpty().NotEqual("");
+            RuleFor(p => p.ToWhere).NotEqual("").WithMessage("This field cannot be left blank");
+            RuleFor(p => p.ToWhere).NotNull().WithMessage("This field cannot be left blank");
+            RuleFor(p => p.ToWhere).NotEmpty().WithMessage("This field cannot be left blank");
+            RuleFor(p => p.WhereFrom).NotEmpty().NotEqual("").WithMessage("This field cannot be left blank"); 
 
         }
     }
